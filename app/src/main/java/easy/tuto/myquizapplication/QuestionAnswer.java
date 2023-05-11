1d9eea2fb29fbda25b4033680622568737907edc
SOHAM KANTI JANA <sohamkantijana@gmail.com>
2023-05-11 20:12:20 +0530

    updating MyQuizApplication-master script commiting on previous date1

--- app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -6,21 +6,27 @@ public class QuestionAnswer {
             "Which company owns the android?",
             "Which one is not the programming language?",
             "Where you are watching this video?",
-            "Which company owns the Apple?"
+            "Which company owns the Apple?",
+            "Which of the following converts Java byte code into Dalvik byte code?",
+            "ADB stands for"
     };
 
     public static String choices[][] = {
             {"Google","Apple","Nokia","Samsung"},
             {"Java","Kotlin","Notepad","Python"},
             {"Facebook","Whatsapp","Instagram","Youtube"},
-            {"Google","Apple","Nokia","Samsung"}
+            {"Google","Apple","Nokia","Samsung"},
+            {"Dalvik converter","Dex compiler","Mobile interpretive compiler (MIC)","None of the above"},
+            {"Android debug bridge", "Android delete bridge", "Android destroy bridge", "None of the above"}
     };
 
     public static String correctAnswers[] = {
             "Google",
             "Notepad",
             "Youtube",
-            "Apple"
+            "Apple",
+            "Dex compiler",
+            "Android debug bridge"
     };
 
 }
