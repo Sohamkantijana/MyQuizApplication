ba57cc3e82b3607bb59de86ab98092868bb23878
Sohamkanti Jana <100026297+Sohamkantijana@users.noreply.github.com>
2023-05-17 23:02:47 +0530

    Update ExampleInstrumentedTest.java
    Add static platform

--- app/src/androidTest/java/easy/tuto/myquizapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/easy/tuto/myquizapplication/ExampleInstrumentedTest.java
@@ -1,7 +1,6 @@
 package easy.tuto.myquizapplication;
 
 import android.content.Context;
-
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
